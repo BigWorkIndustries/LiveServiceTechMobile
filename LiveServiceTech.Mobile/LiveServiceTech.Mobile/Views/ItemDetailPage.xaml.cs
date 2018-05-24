6b605945d780cfe98183c69a74833af70f923561
Vincil Bishop (Surge) <vbishop@surgeforward.com>
2018-05-24 12:34:43 -0500

    Added project MVVM

---
+++ LiveServiceTech.Mobile/LiveServiceTech.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using LiveServiceTech.Mobile.Models;
+using LiveServiceTech.Mobile.ViewModels;
+
+namespace LiveServiceTech.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
