1160a774ebb1621189c6433e3cf94535c1f54c6e
Vincil Bishop (Surge) <vbishop@surgeforward.com>
2018-06-21 10:53:52 -0500

    Updated for angular material and routes

--- LiveServiceTech.Web/live-service-tech/src/app/app.module.ts
+++ LiveServiceTech.Web/live-service-tech/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material/material.module';
+import { RouterModule, Routes } from '@angular/router';
+import {appRoutes} from './app.routes';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
